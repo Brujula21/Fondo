5252f6ef38a6d53b2acbc76527764877040c68d2
Brujula21 <sguarguzdck15@gmail.com>
2026-09-04 13:13:42 -0600

    t

---
+++ Java/ca.js
@@ -0,0 +1,33 @@
+console.log("Hello, world!");
+console.warn("aguas");
+console.error("ay limon");
+let x = 50;
+const Y=6;
+z=x+Y;
+
+if(x==5){
+    console.warn("es 5 el numero de aqui");
+}
+else if(Y!=6){
+    console.error("efectivamente es 6");
+}
+else{
+    console.log("no se que quiera hacer erick");
+}
+
+let numero1 = 20;
+let numero2 = 10;
+let resultado = numero1 + numero2;
+alert("Esto es un mensaje de su patrocinador rayador");
+
+if(resultado > 0){
+    alert("No explota");
+}
+else if(resultado < 0){
+    alert("La bomba explotara");
+}
+
+for(let i = 0; i < 10; i++){
+    alert("erick es comp: " + i);
+}
+
